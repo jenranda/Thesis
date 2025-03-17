15982eb053b110a5eeb02d1d603079325df3f4f9
Jen Rose Tejano <150405731+jenranda@users.noreply.github.com>
2025-03-17 15:31:06 +0800

    Update reports.php

--- Thesis/reports.php
+++ Thesis/reports.php
@@ -75,7 +75,6 @@
         </div>
     </div>
 
-    <!-- Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76Aq4zXbomLwW6fG6UnmyblF9OmKXBmW1R8Zkk5axl7RHEaOsyVj52X1LJq0zB" crossorigin="anonymous"></script>
 </body>
 </html>
